a3f6f98a7285e906da508f6cd158f421c88ea6b0
Anton Skryagin <44167136+m7only@users.noreply.github.com>
2023-05-21 19:14:30 +0700

    Init commit

---
+++ src/main/java/m7/graduatework/GraduateWorkApplication.java
@@ -0,0 +1,13 @@
+package m7.graduatework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduateWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraduateWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/m7/graduatework/GraduateWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package m7.graduatework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduateWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
